8c48b27220cf46c6fa96805417cb02f2e0218e4d
Mohamed-Badr77 <126291482+Mohamed-Badr77@users.noreply.github.com>
2023-04-09 11:15:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 (function (window) {
-    var names = ["Yaakov", "Badr", "Ismail", "Adil", "Khouloud", "Ahmed", "Anas", "Ali", "Soulaiman", "Jim"];
-  
-    for (var i in names) {
-      var firstLetter = names[i].charAt(0).toLowerCase();
-      if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
-      } else {
-        helloSpeaker.speak(names[i]);
-      }
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i in names) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
     }
-  })(window);
-  
+  }
+})(window);
